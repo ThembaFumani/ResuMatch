177a0c7e4e6ff89f627599635114ed909b535dd7
themba <thembamaluleke6@gmail.com>
2025-05-13 12:44:01 +0200

    Might mess this up again

--- ResuMatch.Api/Services/Interfaces/ISkillMatcher.cs
+++
@@ -1,9 +0,0 @@
-using ResuMatch.Api.Models;
-
-namespace ResuMatch.Api.Services.Interfaces
-{
-    public interface ISkillMatcher
-    {
-     AnalysisResult MatchSkills(List<string> resumeSkills, List<string> jobDescriptionSkills);
-    }
-}
