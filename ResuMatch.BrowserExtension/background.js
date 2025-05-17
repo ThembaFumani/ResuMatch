1a96a59c83c5604758a194c32b82de12db51640c
themba <thembamaluleke6@gmail.com>
2025-05-17 18:53:33 +0200

    Initial commit: Adding BrowserExtension project

---
+++ ResuMatch.BrowserExtension/background.js
@@ -0,0 +1,26 @@
+  function sendDataToBackend(fileData, fileName, jobDescription, sendResponse) {
+    const formData = new FormData();
+    const blob = new Blob([fileData]);
+    formData.append("file", blob, fileName); // Key must be "file"
+    formData.append("jobDescription", jobDescription); // Key must be "jobDescription"
+    const apiUrl = "http://localhost:5250/api/resume/upload";
+  
+    fetch(apiUrl, {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => {
+        if (!response.ok) {
+          return response.text().then((text) => {
+            throw new Error(`HTTP error! Status: ${response.status}, Response: ${text}`);
+          });
+        }
+        return response.json();
+      })
+      .then((data) => {
+        sendResponse({ success: true, results: data });
+      })
+      .catch((error) => {
+        sendResponse({ success: false, error: error.message });
+      });
+  }
